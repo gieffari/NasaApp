e1ea8a4112c5ab59055a7009a3131700dcf9e356
gieffari <48472651+gieffari@users.noreply.github.com>
2019-05-12 07:17:31 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=0WT6PoLA0aAWgQbWdKQZcWEdAIX1uvbOkjD3mHd1";
         
         $("#loadImage").click(function(){
             $.ajax({
